89933064ceb3f210f325e7482593a18e3a2f7c0a
ngoctu280801 <63794942+ngoctu280801@users.noreply.github.com>
2024-05-14 21:35:27 +0700

    feat: express rate limit

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc from 'swagger-jsdoc'
 import { envConfig } from './constants/config'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const options: swaggerJSDoc.Options = {
   definition: {
@@ -52,6 +53,17 @@ databaseService.connect().then(() => {
 })
 
 const app = express()
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 const httpServer = createServer(app)
 
